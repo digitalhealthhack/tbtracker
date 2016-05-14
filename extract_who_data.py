9000d82151a5abe98510b471832eff9c5e20b121
Richie@daingean <richieharrington@ymail.com>
2016-05-14 16:45:24 +0100

    Cleaned up data processing files

--- extract_who_data.py
+++ extract_who_data.py
@@ -50,7 +50,46 @@ def data_to_db(data):
                             ))
 
 
-def read_who_excel():
+def data_dictionary_to_db(data):
+    
+    with con:
+
+        cur = con.cursor()
+
+        sql = "INSERT INTO who_data_dictionary (variable_name, dataset, code_list, definition) VALUES (%s, %s, %s, %s)"
+
+        cur.execute(sql, (data['name'], data['dataset'], data['code_list'], data['definition']))
+
+
+def extract_who_data_dictionary():
+
+    # Define Excel file, open workbook and get main worksheet
+    filename = 'Data/WHO/TB_data_dictionary_2016-05-07.xls' 
+    book = xlrd.open_workbook(filename)
+    main_sheet = book.sheet_by_index(0)
+
+    # Loop through all rows, starting from second row (first is headers)
+    for row_id in range(1, main_sheet.nrows):  
+
+        print ('-'*40)
+        print ('Row: %s' % row_id)
+
+        for col_id in range(0, main_sheet.ncols):  # Iterate through columns
+            cell_obj = main_sheet.cell(row_id, col_id)  # Get cell object by row, col
+            print ('Column: [%s] cell_obj: [%s]' % (col_id, cell_obj))
+
+        # Get specific datapoints to add to database
+        data = dict()
+        data['name'] = main_sheet.cell(row_id, 0).value
+        data['dataset'] = main_sheet.cell(row_id, 1).value
+        data['code_list'] = main_sheet.cell(row_id, 2).value
+        data['definition'] = main_sheet.cell(row_id, 3).value
+
+        pprint(data)
+        data_dictionary_to_db(data)
+
+
+def extract_who_data():
 
     # Define Excel file, open workbook and get main worksheet
     filename = 'Data/WHO/TB_burden_countries_2016-05-07.xls' 
@@ -194,7 +233,8 @@ def read_who_excel():
 
 def main():
     
-    data = read_who_excel()
+    extract_who_data_dictionary()
+    extract_who_data()
 
 
 if __name__ == '__main__':
--- extract_worldbank_data.py
+++ extract_worldbank_data.py
@@ -20,7 +20,7 @@ con = mdb.connect(config['hostname'],
                   )
 
     
-def data_to_db(data):
+def who_data_to_db(data):
     
     with con:
 
@@ -29,7 +29,7 @@ def data_to_db(data):
         cur.execute(sql, (data['country_code'], data['year'], data['tb_count']))
 
 
-def read_who_excel_metadata():
+def extract_who_data():
 
     # Define Excel file, open workbook and get main worksheet
     filename = 'Data/WorldBank/API_SH.TBS.INCD_DS2_en_excel_v2.xls' 
@@ -59,12 +59,75 @@ def read_who_excel_metadata():
                     'tb_count': count}
 
             pprint(data)
-            data_to_db(data)
+            who_data_to_db(data)
+
+
+def who_metadata_to_db(data):
+
+    with con:
+
+        cur = con.cursor()
+
+        sql = "INSERT INTO world_bank_metadata_countries (country_code, region, income_group, notes, tablename) VALUES (%s, %s, %s, %s, %s)"
+
+        cur.execute(sql, (data['country_code'], data['region'], data['income_group'], data['notes'], data['tablename']))
+
+
+def extract_who_metadata():
+
+   # Define Excel file, open workbook and get main worksheet
+    filename = 'Data/WorldBank/API_SH.TBS.INCD_DS2_en_excel_v2.xls' 
+    book = xlrd.open_workbook(filename)
+    main_sheet = book.sheet_by_index(1)
+
+    for row_id in range(1, main_sheet.nrows):  
+
+        print ('-'*40)
+        print ('Row: %s' % row_id)
+
+        for col_id in range(0, main_sheet.ncols):  # Iterate through columns
+            cell_obj = main_sheet.cell(row_id, col_id)  # Get cell object by row, col
+            print ('Column: [%s] cell_obj: [%s]' % (col_id, cell_obj))
+
+        if main_sheet.cell(row_id, 0).value:
+            country_code = main_sheet.cell(row_id, 0).value
+        else:
+            country_code = None
+
+        if main_sheet.cell(row_id, 1).value:
+            region = main_sheet.cell(row_id, 1).value
+        else:
+            region = None
+
+        if main_sheet.cell(row_id, 2).value:            
+            income_group = main_sheet.cell(row_id, 2).value
+        else:
+            income_group = None
+
+        if main_sheet.cell(row_id, 3).value:
+            notes = main_sheet.cell(row_id, 3).value
+        else:
+            notes = None
+
+        if main_sheet.cell(row_id, 4).value:
+            tablename = main_sheet.cell(row_id, 4).value
+        else:
+            tablename = None
+
+        data = {'country_code': country_code, 
+                'region': region, 
+                'income_group': income_group, 
+                'notes': notes,
+                'tablename': tablename}
+        
+        pprint(data)
+        data_to_db(data)
 
 
 def main():
     
-    data = read_who_excel_metadata()
+    extract_who_metadata()
+    extract_who_data()
 
 
 if __name__ == '__main__':
